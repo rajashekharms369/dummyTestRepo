d771023d90fb5f7715dcbb55c6cccb9ca0da1303
rajashekharms369 <rajmkumar369@gmail.com>
2024-03-03 15:27:20 +0530

    updated with http service

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 console.log("Hello world");
 
 console.log("Another commit");
@@ -6,3 +8,11 @@ console.log("Commit from branch feature");
 
 console.log("Hello world");
 console.log("new branch004");
+
+
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope, $http){
+    $http.get("welcome.htm").then(function(respone){
+        $scope.myWelcome = response.data;
+    });
+});
