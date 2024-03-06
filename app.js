4bd5c7b644854188fa7edf4beca1ef9040935bbd
rajashekharms369 <rajmkumar369@gmail.com>
2024-03-06 09:31:46 +0530

    angular interval service

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ console.log("Commit from branch feature");
 console.log("Hello world");
 console.log("new branch004");
 
-var app = angular.module("myApp", []);
-app.controller("myCtrl", function($scope, $timeout){
-    $scope.myHeader = "Hello world";
-    $timeout(function(){
-        $scope.myHeader = "How are you today";
-    },2000)
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $timeout) {
+  $scope.myHeader = "Hello World!";
+  $timeout(function () {
+      $scope.myHeader = "How are you today?";
+  }, 2000);
 });
