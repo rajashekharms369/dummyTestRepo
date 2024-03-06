2cab5518712c7c7bbec2af08815f0a398fb8e6b6
rajashekharms369 <rajmkumar369@gmail.com>
2024-03-06 09:04:39 +0530

    angularjs timeout service

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ console.log("Commit from branch feature");
 
 console.log("Hello world");
 console.log("new branch004");
+
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope, $timeout){
+    $scope.myHeader = "Hello world";
+    $timeout(function(){
+        $scope.myHeader = "How are you today";
+    },2000)
+});
